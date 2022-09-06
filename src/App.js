bd53da218bfe2d6e7fac2807742940eef00e6af1
Henry Kondratko <henry.kondratko@gmail.com>
2022-09-05 21:14:39 -0400

    save to local storage WIP

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
-import todo from "./components/Todo";
 
 function App() {
     const [inputText, setInputText] = useState("");
     const [todos, setTodos] = useState([]);
-    const [status, setStatus] = useState('all');
+    const [status, setStatus] = useState("all");
     const [filteredTodos, setFilteredTodos] = useState([]);
 
     useEffect(() => {
+        getLocalTodos();
+    }, []);
+
+    useEffect(() => {
         filterHandler();
+        saveLocalTodos();
     }, [todos, status]);
 
     const filterHandler = () => {
@@ -26,7 +30,19 @@ function App() {
                 setFilteredTodos(todos)
                 break;
         }
-    }
+    };
+
+    const saveLocalTodos = () => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    };
+    const getLocalTodos = () => {
+        if (localStorage.getItem("todos") === null){
+            localStorage.setItem("todos", JSON.stringify([]));
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem("todos"));
+            setTodos(todoLocal);
+        }
+    };
     return (
         <div className="App">
             <header>
